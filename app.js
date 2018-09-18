ae9692fd3172490f7c28492568f1c460d72462c9
Beor18 <fer.ragnar19@gmail.com>
2018-09-18 10:53:08 -0300

    se guarda en base de datos

--- app.js
+++ app.js
@@ -3,19 +3,32 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const config = require('./bin/db');
+
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+    next();
+});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
 
 app.use('/', indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const scribble = require('scribbletune');
+const Archivo = require('../models/Archivo');
 
-router.get('/', (req, res, next) => {
-
-    res.render('index', { title: 'Express' });
+router.get('/', async(req, res, next) => {
+    const musica = await Archivo.find();
+    res.render('index', { title: 'Express', musica });
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
+    // Se crea el archivo MIDI
     let clip = scribble.clip({
         notes: req.body.notas,
         pattern: req.body.patterns.repeat(8)
     });
+    let arch = './public/chords-' + Date.now() + '-.mid';
+    scribble.midi(clip, arch);
 
-    scribble.midi(clip, './public/chords-' + Date.now() + '-.mid');
-    res.redirect('/');
+    // Se guarda en la base de datos las notas
+    let mid = new Archivo({
+        notes: req.body.notas,
+        pattern: req.body.patterns,
+        arch: arch
+    });
+
+    await mid.save(() => {
+        res.send("Archivo guardado con éxito en la DB!");
+        console.log("Archivo guardado con éxito en la DB!");
+    });
 });
 
 module.exports = router;
