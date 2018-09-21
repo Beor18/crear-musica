9397a8bc9e342d6b832691c8f71f2b14ac4aacc9
Beor18 <fer.ragnar19@gmail.com>
2018-09-20 22:13:04 -0300

    convertimos midi a mp3 con sonido del emulador

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,12 @@ const scribble = require('scribbletune');
 const Archivo = require('../models/Archivo');
 const path = require('path');
 
+const fs = require('fs');
+const lame = require('lame');
+const OPL3 = require('opl3').OPL3;
+const LAA = require('opl3').format.LAA;
+const Player = require('opl3').Player
+
 router.get('/', async(req, res, next) => {
     const musica = await Archivo.find();
     res.render('index', { title: 'Express', musica });
@@ -18,11 +24,32 @@ router.post('/', async(req, res) => {
     let arch = path.join('chords-' + Date.now() + '-.mid');
     scribble.midi(clip, './public/' + arch);
 
+    // Procesamos archivo mid y convertimos a mp3 utilizando los sonidos del emulador Yamaha Opl3
+    const player = new Player(LAA);
+    const link = 'yamaha-' + Date.now() + '-.mp3';
+    const file = fs.createWriteStream('./public/' + link);
+    const encoder = new lame.Encoder({
+        // Input/Entrada
+        channels: 2, // 2 channels (left and right)
+        bitDepth: 16, // 16-bit samples
+        sampleRate: 49700 // 49,700 Hz sample rate
+    });
+
+    player.pipe(encoder);
+    encoder.pipe(file);
+
+    player.load(fs.readFileSync('./public/' + arch));
+    player.on('progress', function(value) {
+        //process.stdout.write('# \n ');
+        console.log('Archivo .mp3 generado con ÉXITO! por Emulador Yamaha OPL3 ');
+    });
+
     // Se guarda en la base de datos las notas
     let mid = new Archivo({
         notes: req.body.notas,
         pattern: req.body.patterns,
-        arch: arch
+        arch: arch,
+        link: link
     });
 
     await mid.save(() => {
--- test-sound.js
+++ test-sound.js
@@ -1,3 +1,22 @@
-const Tone = require('tone')
+const fs = require('fs');
+const lame = require('lame');
+const OPL3 = require('opl3').OPL3;
+const LAA = require('opl3').format.LAA;
+const Player = require('opl3').Player;
 
-console.log(Tone.AmplitudeEnvelope);
+const player = new Player(LAA);
+const file = fs.createWriteStream('./public/testing.mp3');
+const encoder = new lame.Encoder({
+    // input
+    channels: 2, // 2 channels (left and right)
+    bitDepth: 16, // 16-bit samples
+    sampleRate: 49700 // 49,700 Hz sample rate
+});
+
+player.pipe(encoder);
+encoder.pipe(file);
+
+player.load(fs.readFileSync('./public/chords-1537485160216-.mid'));
+player.on('progress', function(value) {
+    process.stdout.write('Bien!');
+});
